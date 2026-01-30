1b3dc21faa0be78a0d9b4a96d163f4b5ec8ef0a4
Your Name <deshkarchinmay42@gmail.com>
2026-01-31 03:21:32 +0530

    hide navbar while on feedback page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthService } from './services/auth.service';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'crmfront';
-  constructor(private authService : AuthService, private router: Router) {}
+  showNavbar: boolean = true;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        console.log('Current URL:', url);
+
+        // ✅ GUARANTEED CHECK
+        this.showNavbar = !url.includes('feedback');
+      }
+    });
+  }
 
   logOut() {
     this.authService.logout();
     this.router.navigate(['/login']);
-
   }
 }
